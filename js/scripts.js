414e5d5bb038e8a84f8ca58e978b9a82ce8fdb3e
Ian Cook Westgate <iwestgate@hotmail.com>
2019-01-18 09:18:29 -0800

    Add the beginnings of javascript functionality to the questions.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+
+    var total = 0;
+    var nameInput = $("input#name").val();
+    var question1Input = parseInt($("input#question1").val());
+    var question2Input = parseInt($("input#question2").val());
+    var question3Input = parseInt($("input#question3").val());
+    var question4Input = parseInt($("input#question4").val());
+    var question5Input = parseInt($("input#question5").val());
+  })
+
+})
